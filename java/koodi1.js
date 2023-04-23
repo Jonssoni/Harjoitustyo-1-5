374c0eab2a00e37d4f0be23d79e7b6d54e673314
Jonssoni <n2nejo00@students.oamk.fi>
2023-04-23 20:52:02 +0300

    harjoitustyö aloitettu tekemällä navbar yms.

---
+++ java/koodi1.js
@@ -0,0 +1,30 @@
+document.querySelector("button").addEventListener("click", addNote);
+let body = document.querySelector("body");
+
+let note = document.getElementById("viesti");
+let checkbox = document.getElementById("important");
+
+
+
+//luodaan funtio addtext
+function addNote() {
+
+    let h3 = document.createElement("h3");
+
+    let p = document.createElement("p");
+    p.textContent = viesti.value;
+
+    //onko checkbox painettuna vai ei
+
+    if(checkbox.checked){
+        h3.classList.add("important");
+        p.classList.add("important");
+    }
+
+    //kaikki elementit bodyn sisälle
+    body.append(h3,p,document.createElement("message"));
+}
+
+let nimi = document.getElementById("username").value;
+
+document.getElementById("body").innerHTML = nimi;
